2213f5973005cd478107c85a0a9fd41c1d4b0905
parthu19 <parthk.0101@gmail.com>
2022-09-09 21:44:40 +0530

    testing...

--- src/main/java/com/spring/boot/docker/controller/MyController.java
+++ src/main/java/com/spring/boot/docker/controller/MyController.java
@@ -8,6 +8,6 @@ public class MyController {
 
     @GetMapping("/test-docker")
     public String getData(){
-        return "in docker tutorial project of play java";
+        return "in docker tutorial project of play java   parhtkl...";
     }
 }
